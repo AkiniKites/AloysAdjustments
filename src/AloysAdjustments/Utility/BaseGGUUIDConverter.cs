9a67fc6f64bcf3a7626c84cc2bc1566ca57de772
AkiniKites <akinikites@gmail.com>
2021-01-01 21:02:07 -0500

    Cleanup converter

--- src/AloysAdjustments/Utility/BaseGGUUIDConverter.cs
+++ src/AloysAdjustments/Utility/BaseGGUUIDConverter.cs
@@ -17,41 +17,14 @@ namespace AloysAdjustments.Utility
                 return null;
 
             var data = reader.Value as string;
-            if (data.Length != 32)
-                throw new HzdException($"Invalid BaseGGUUID data length: {data.Length}");
-
-            var i = 0;
-            var id = new BaseGGUUID();
-            id.Data3 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data2 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data1 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data0 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-
-            id.Data5 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data4 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-
-            id.Data7 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data6 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-
-            id.Data8 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data9 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data10 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data11 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data12 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data13 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data14 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-            id.Data15 = Convert.ToByte(data.Substring((i++ * 2), 2), 16);
-
-            return id;
+            return BaseGGUUID.FromString(data);
         }
 
         public override void WriteJson(JsonWriter writer, [AllowNull] BaseGGUUID value, JsonSerializer serializer)
         {
             if (value != null)
             {
-                var str = $"{value.Data3:X2}{value.Data2:X2}{value.Data1:X2}{value.Data0:X2}{value.Data5:X2}{value.Data4:X2}{value.Data7:X2}" +
-                    $"{value.Data6:X2}{value.Data8:X2}{value.Data9:X2}{value.Data10:X2}{value.Data11:X2}{value.Data12:X2}{value.Data13:X2}{value.Data14:X2}{value.Data15:X2}";
-                writer.WriteValue(str);
+                writer.WriteValue(value.ToString());
             }
             else
             {
