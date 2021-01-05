296654bdf9f3d436cd75de7d2a8fee1507a58327
AkiniKites <akinikites@gmail.com>
2021-01-05 11:51:14 -0500

    Add hzd cloner

---
+++ src/AloysAdjustments/Utility/HzdCloner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Decima;
+
+namespace AloysAdjustments.Utility
+{
+    public class HzdCloner
+    {
+        public static T Clone<T>(T obj)
+        {
+            if (obj == null) return default(T);
+
+            using var ms = new MemoryStream();
+            using var bw = new BinaryWriter(ms);
+            using var br = new BinaryReader(ms);
+
+            RTTI.SerializeType(bw, obj);
+            ms.Position = 0;
+            return RTTI.DeserializeType<T>(br);
+        }
+    }
+}
