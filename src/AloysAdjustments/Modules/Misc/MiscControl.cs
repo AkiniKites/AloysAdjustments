58ada6558def05122434e4b00961721d11efc0b0
AkiniKites <akinikites@gmail.com>
2021-01-04 21:03:52 -0500

    Add misc module

---
+++ src/AloysAdjustments/Modules/Misc/MiscControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AloysAdjustments.Modules.Misc
+{
+    public class MiscControl : ModuleBase
+    {
+        public override string ModuleName => "Misc";
+
+        public override Task LoadPatch(string path) => null;
+        public override Task CreatePatch(string patchDir) => null;
+        public override Task Initialize() => null;
+        public override bool ValidateChanges() => true;
+
+        protected override void Reset_Click() { }
+        protected override void ResetSelected_Click() { }
+    }
+}
